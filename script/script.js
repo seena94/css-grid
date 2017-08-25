310881c161b24ea2689978b40a8b388d8791fd11
Seena James <you@example.com>
2017-08-25 18:40:06 +0530

    clickevents added

--- script/script.js
+++ script/script.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
 	$('ul a').click(function(e) {
     hideContentDivs();
     var tmp_div = $(this).parent('li').index();
-    $('.content-wrapper div').eq(tmp_div).show();
+    $('.grid-content-wrapper .content').eq(tmp_div).show();
   });
 
 	function hideContentDivs(){
-	  $('.content-wrapper div').each(function(){
+	  $('.grid-content-wrapper .content').each(function(){
 		  $(this).hide();
 		});
 	}
